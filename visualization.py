e2bf2e586c515253309a61b20b2fd3b84412ffeb
BijlesAanHuis <55694950+BijlesAanHuis@users.noreply.github.com>
2025-05-19 16:24:47 +0300

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -45,37 +45,36 @@ q25 = Quantiles['lessons_per_relation'].quantile(0.25)
 q75 = Quantiles['lessons_per_relation'].quantile(0.75)
 
 Tutor['quantile_labels'] = Tutor['lessons_per_relation'].apply(categorize)
-Tutor['recent_lesson'] = Tutor['recent_lesson'].apply(
-    lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S.%f").date() if isinstance(x, str) else None
-)
+Tutor['recent_lesson'] = Tutor['recent_lesson'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S.%f").date() if isinstance(x, str) else None)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     filtered_df = Courses.copy()
     temp_df = Tutor[['tutor', 'number_of_relations', 'lessons_per_relation', 'excluded_from_search', 'recent_lesson']]
     filtered_df = pd.merge(filtered_df, temp_df, how='left', on='tutor')
+
     countries = Courses['country'].unique().tolist()
     school_levels = school_years = school_types = course_names = availability = tutor_types = excluded = []
-    map_html = df_html = selected_country = None
-    selected_school_levels = selected_school_years = selected_school_types = selected_course_names = []
-    selected_availabilities = selected_tutor_types = selected_excluded = []
-    lessons_per_relation = no_lesson_tutor = start_date = end_date = None
-    use_location_filter = latitude_input = longitude_input = distance_km = None
+    map_html = None
+    selected_country = None
+    selected_school_levels = selected_school_years = selected_school_types = selected_course_names = selected_availabilities = lessons_per_relation = no_lesson_tutor = selected_tutor_types = selected_excluded = []
+    start_date = end_date = None
+    df_html = None
 
     if request.method == 'POST':
         selected_country = request.form.get('country')
+
         if selected_country:
             filtered_df = filtered_df[filtered_df['country'] == selected_country]
-
             school_levels = sorted(filtered_df['school_level'].fillna('-Empty-').unique().tolist(), key=lambda x: (x == '-Empty-', x))
             school_years = filtered_df['school_year'].fillna('-Empty-').unique().tolist()
-            school_years = sorted([x for x in school_years if isinstance(x, float)]) + [x for x in school_years if isinstance(x, str)]
+            school_years = sorted(x for x in school_years if isinstance(x, float)) + [x for x in school_years if isinstance(x, str)]
             school_types = sorted(filtered_df['school_type'].fillna('-Empty-').unique().tolist(), key=lambda x: (x == '-Empty-', x))
             course_names = sorted(filtered_df['course_name'].fillna('-Empty-').unique().tolist(), key=lambda x: (x == '-Empty-', x))
             availability = filtered_df['availability'].fillna('-Empty-').unique().tolist()
             excluded = filtered_df['excluded_from_search'].fillna('-Empty').unique().tolist()
             tutor_types = filtered_df['tutor_category'].fillna('-Empty-').unique().tolist()
-            tutor_types = sorted([x for x in tutor_types if isinstance(x, float)]) + [x for x in tutor_types if isinstance(x, str)]
+            tutor_types = sorted(x for x in tutor_types if isinstance(x, float)) + [x for x in tutor_types if isinstance(x, str)]
 
             selected_school_levels = request.form.getlist('school_level')
             selected_school_years = convert_float_list_to_int(request.form.getlist('school_year'))
@@ -87,6 +86,7 @@ def index():
             no_lesson_tutor = request.form.get('no_lesson_tutor')
             start_date = request.form.get('start_date')
             end_date = request.form.get('end_date')
+
             use_location_filter = request.form.get('use_location_filter')
             latitude_input = request.form.get('latitude_input')
             longitude_input = request.form.get('longitude_input')
@@ -108,58 +108,42 @@ def index():
 
             try:
                 start_date = datetime.strptime(start_date, "%Y-%m-%d").date() if start_date else None
+            except ValueError:
+                start_date = None
+
+            try:
                 end_date = datetime.strptime(end_date, "%Y-%m-%d").date() if end_date else None
             except ValueError:
-                start_date = end_date = None
+                end_date = None
 
             if selected_school_levels:
-                if '-Empty-' in selected_school_levels:
-                    filtered_df = filtered_df[filtered_df['school_level'].isnull() | filtered_df['school_level'].isin(selected_school_levels)]
-                else:
-                    filtered_df = filtered_df[filtered_df['school_level'].isin(selected_school_levels)]
+                filtered_df = filtered_df[filtered_df['school_level'].isnull() | filtered_df['school_level'].isin(selected_school_levels)] if '-Empty-' in selected_school_levels else filtered_df[filtered_df['school_level'].isin(selected_school_levels)]
 
             if selected_school_years:
-                if '-Empty-' in selected_school_years:
-                    filtered_df = filtered_df[filtered_df['school_year'].isnull() | filtered_df['school_year'].isin(selected_school_years)]
-                else:
-                    filtered_df = filtered_df[filtered_df['school_year'].isin(selected_school_years)]
+                filtered_df = filtered_df[filtered_df['school_year'].isnull() | filtered_df['school_year'].isin(selected_school_years)] if '-Empty-' in selected_school_years else filtered_df[filtered_df['school_year'].isin(selected_school_years)]
 
             if selected_school_types:
-                if '-Empty-' in selected_school_types:
-                    filtered_df = filtered_df[filtered_df['school_type'].isnull() | filtered_df['school_type'].isin(selected_school_types)]
-                else:
-                    filtered_df = filtered_df[filtered_df['school_type'].isin(selected_school_types)]
+                filtered_df = filtered_df[filtered_df['school_type'].isnull() | filtered_df['school_type'].isin(selected_school_types)] if '-Empty-' in selected_school_types else filtered_df[filtered_df['school_type'].isin(selected_school_types)]
 
             if selected_course_names:
-                if '-Empty-' in selected_course_names:
-                    filtered_df = filtered_df[filtered_df['course_name'].isnull() | filtered_df['course_name'].isin(selected_course_names)]
-                else:
-                    filtered_df = filtered_df[filtered_df['course_name'].isin(selected_course_names)]
+                filtered_df = filtered_df[filtered_df['course_name'].isnull() | filtered_df['course_name'].isin(selected_course_names)] if '-Empty-' in selected_course_names else filtered_df[filtered_df['course_name'].isin(selected_course_names)]
 
             if selected_availabilities:
-                if '-Empty-' in selected_availabilities:
-                    filtered_df = filtered_df[filtered_df['availability'].isnull() | filtered_df['availability'].isin(selected_availabilities)]
-                else:
-                    filtered_df = filtered_df[filtered_df['availability'].isin(selected_availabilities)]
+                filtered_df = filtered_df[filtered_df['availability'].isnull() | filtered_df['availability'].isin(selected_availabilities)] if '-Empty-' in selected_availabilities else filtered_df[filtered_df['availability'].isin(selected_availabilities)]
 
             if selected_tutor_types:
-                if '-Empty-' in selected_tutor_types:
-                    filtered_df = filtered_df[filtered_df['tutor_category'].isnull() | filtered_df['tutor_category'].isin(selected_tutor_types)]
-                else:
-                    filtered_df = filtered_df[filtered_df['tutor_category'].isin(selected_tutor_types)]
+                filtered_df = filtered_df[filtered_df['tutor_category'].isnull() | filtered_df['tutor_category'].isin(selected_tutor_types)] if '-Empty-' in selected_tutor_types else filtered_df[filtered_df['tutor_category'].isin(selected_tutor_types)]
 
             if no_lesson_tutor:
-                temp_df = filtered_df.fillna({'lessons_per_relation': 0})
-                filtered_df = temp_df[temp_df['lessons_per_relation'] >= lessons_per_relation]
+                filtered_df = filtered_df.fillna({'lessons_per_relation': 0})
+                filtered_df = filtered_df[filtered_df['lessons_per_relation'] >= lessons_per_relation] if lessons_per_relation is not None else filtered_df
             else:
-                temp_df = filtered_df[pd.notna(filtered_df['lessons_per_relation'])]
-                filtered_df = temp_df[temp_df['lessons_per_relation'] >= lessons_per_relation]
+                if lessons_per_relation is not None:
+                    filtered_df = filtered_df[pd.notna(filtered_df['lessons_per_relation'])]
+                    filtered_df = filtered_df[filtered_df['lessons_per_relation'] >= lessons_per_relation]
 
             if selected_excluded:
-                if '-Empty-' in selected_excluded:
-                    filtered_df = filtered_df[filtered_df['excluded_from_search'].isnull() | filtered_df['excluded_from_search'].isin(selected_excluded)]
-                else:
-                    filtered_df = filtered_df[filtered_df['excluded_from_search'].isin(selected_excluded)]
+                filtered_df = filtered_df[filtered_df['excluded_from_search'].isnull() | filtered_df['excluded_from_search'].isin(selected_excluded)] if '-Empty-' in selected_excluded else filtered_df[filtered_df['excluded_from_search'].isin(selected_excluded)]
 
             if start_date:
                 filtered_df = filtered_df[filtered_df['recent_lesson'] >= start_date]
@@ -190,21 +174,17 @@ def index():
 
                 for _, row in Tutor_filtered.iterrows():
                     unique_id = f"{row['tutor']}"
-
                     circle = folium.Circle(
                         location=(row['latitude'], row['longitude']),
                         radius=row['max_travel_distance'] * 1000,
-                        popup=f"""<div class='circle-popup'>Tutor: {row['tutor']}<br>Availability: {row['availability']}<br>Tutor type: {row['tutor_category']}<br>Excluded: {row['excluded_from_search']}<br>Total lessons: {row['total_lessons']}<br>Per relation: {row['lessons_per_relation']}<br>Link: <a href=\"{'https://bijlesaanhuis.nl/profiel/' if row['country'] == 'nl' else 'https://lernigo.de/profil/'}{row['tutor']}\">Profile</a></div>""",
-                        fill=True,
-                        fill_opacity=0,
+                        popup=f"<div class='circle-popup'>Tutor: {row['tutor']}<br>Availability: {row['availability']}<br>Tutor type: {row['tutor_category']}<br>Excluded: {row['excluded_from_search']}<br>Total lessons: {row['total_lessons']}<br>Per relation: {row['lessons_per_relation']} lesson(s)<br>Link: <a href='https://{'bijlesaanhuis.nl' if row['country'] == 'nl' else 'lernigo.de'}/profiel/{row['tutor']}' target='_blank'>Profile</a></div>",
+                        fill=True, fill_opacity=0
                     ).add_to(tutors_map)
-
                     marker = folium.Marker(
                         location=(row['latitude'], row['longitude']),
-                        popup=f"""<div class='marker-popup'>Tutor: {row['tutor']}<br>Availability: {row['availability']}<br>Tutor type: {row['tutor_category']}<br>Excluded: {row['excluded_from_search']}<br>Total lessons: {row['total_lessons']}<br>Per relation: {row['lessons_per_relation']}<br>Link: <a href=\"{'https://bijlesaanhuis.nl/profiel/' if row['country'] == 'nl' else 'https://lernigo.de/profil/'}{row['tutor']}\">Profile</a></div>""",
+                        popup=f"<div class='marker-popup'>Tutor: {row['tutor']}<br>Availability: {row['availability']}<br>Tutor type: {row['tutor_category']}<br>Excluded: {row['excluded_from_search']}<br>Total lessons: {row['total_lessons']}<br>Per relation: {row['lessons_per_relation']} lesson(s)<br>Link: <a href='https://{'bijlesaanhuis.nl' if row['country'] == 'nl' else 'lernigo.de'}/profiel/{row['tutor']}' target='_blank'>Profile</a></div>",
                         icon=folium.Icon(color='red', icon='info-sign')
                     ).add_to(tutors_map)
-
                     marker.options["customId"] = unique_id
                     circle.options["customId"] = unique_id
 
